03da48111ac83d5c861a2fbfe85d4dc9cc41c5e3
Wong Liong Hung <lhwong@gmail.com>
2019-10-15 22:55:39 +0800

    Update KeycloakResourceOwner.php
    Add Roles

--- src/Provider/KeycloakResourceOwner.php
+++ src/Provider/KeycloakResourceOwner.php
@@ -52,6 +52,51 @@ class KeycloakResourceOwner implements ResourceOwnerInterface
     {
         return $this->response['name'] ?: null;
     }
+    
+        /**
+    /**
+     * Get resource owner roles for all clients
+     *
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->response['roles'] ?: [];
+    }
+    /**
+     * Get resource owner roles for given client
+     *
+     * @param string  $client
+     *
+     * @return array
+     */
+    public function getRolesForClient($client)
+    {
+        return isset($this->response['roles'][$client]['roles']) ? $this->response['roles'][$client]['roles'] : [];
+    }
+    /**
+     * Check if resource owner has a given role for given client
+     *
+     * @param string  $client
+     * @param string  $role
+     *
+     * @return boolean
+     */
+    public function hasRoleForClient($client, $role)
+    {
+        return in_array($role, $this->getRolesForClient($client));
+    }
+    /**
+     * Check if resource owner has a access to a given client (have at least one role)
+     *
+     * @param string  $client
+     *
+     * @return boolean
+     */
+    public function hasAccessToClient($client)
+    {
+        return !empty($this->getRolesForClient($client));
+    }
 
     /**
      * Return all of the owner details available as an array.
